4ead4f3c00ea5e7471bf582ca209acc94f760faf
Administrator <2580855214@qq.com>
2023-09-02 16:15:14 +0800

    更新fabric.mod.json

---
+++ src/main/java/org/windy/keybindins/KeybindinsInitializer.java
@@ -0,0 +1,10 @@
+package org.windy.keybindins;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KeybindinsInitializer implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Your client initialization code goes here
+    }
+}
